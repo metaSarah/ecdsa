65a9887ce77c984339d0831fb93deddaf8029106
metaSarah <84154961+metaSarah@users.noreply.github.com>
2022-02-02 12:07:30 -0500

    update sign

--- sign.py
+++ sign.py
@@ -7,8 +7,8 @@ from hashlib import sha256
 def sign(m):
 	sk, pk = keys.gen_keypair(curve.secp256k1)
 	public_key = pk
-	
-	r, s = ecdsa.sign(m,sk,curve.secp256k1,openssl_sha256,False)
+
+	r, s = ecdsa.sign(m, sk, hashfunc=sha3_256)
 	r = 0
 	s = 0
 
