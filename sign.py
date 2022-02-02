0500ac5e47dc7e61cad9b2d0968e18a396857e54
metaSarah <84154961+metaSarah@users.noreply.github.com>
2022-02-02 11:56:38 -0500

    initial commit

---
+++ sign.py
@@ -0,0 +1,25 @@
+from fastecdsa.curve import secp256k1
+from fastecdsa.keys import export_key, gen_keypair
+
+from fastecdsa import curve, ecdsa, keys, point
+from hashlib import sha256
+
+def sign(m):
+	#generate public key
+	#Your code here
+  sk, pk = keys.gen_keypair(curve.secp256k1)
+	public_key = pk
+
+	#generate signature
+	#Your code here
+
+  r, s = fastecdsa.ecdsa.sign(m,sk,curve.secp256k1,openssl_sha256,False)
+	r = 0
+	s = 0
+
+	assert isinstance( public_key, point.Point )
+	assert isinstance( r, int )
+	assert isinstance( s, int )
+	return( public_key, [r,s] )
+
+
